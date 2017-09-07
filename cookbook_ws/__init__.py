2fad4a645576d860e95a658f1dd4d9055f1cb44d
Andrew Kuck <ajkuck@gmail.com>
2017-09-07 06:58:33 -0500

    Added comments.

--- cookbook_ws/__init__.py
+++ cookbook_ws/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# There's currently nothing necessary in this config file, but I expect we'll need it in the future.
 app.config.from_pyfile('../main.ini')
 
 import cookbook_ws.main
--- cookbook_ws/main.py
+++ cookbook_ws/main.py
@@ -5,9 +5,17 @@ from cookbook_ws import app
 
 @app.route("/")
 def welcome():
+    """
+    Main entry point, this method returns the default page for the whole site.
+    """
     return render_template("index.html")
 
 
 @app.route("/random")
 def random_recipe():
+    """
+    This method returns a sample recipe page.
+
+    TODO: Once we've got the backend implemented, we can change this method to serve a random recipe.
+    """
     return render_template("recipe_page.html")
