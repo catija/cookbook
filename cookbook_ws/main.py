57c4760d5acb14e0b439c6777a1d4326496064fc
Andrew Kuck <ajkuck@gmail.com>
2017-09-16 14:15:28 -0500

    Made export save as a file, and added an import method for uploading the file.  No DB changes on upload yet.

--- cookbook_ws/main.py
+++ cookbook_ws/main.py
@@ -1,5 +1,6 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request, url_for
 from sqlalchemy import inspect
+from werkzeug.utils import redirect
 
 from cookbook_ws import app, orm, db
 from cookbook_ws.orm import RecipeType, Recipe
@@ -78,7 +79,40 @@ def export():
     recipes = Recipe.query.order_by(Recipe.create_date)
 
     recipe_dicts = [r.serialize for r in recipes]
-    return jsonify(recipe_dicts)
+
+    response = jsonify(recipe_dicts)
+    response.headers['Content-Disposition'] = 'attachment; filename=margin_recipes.json'
+    response.mimetype = 'text/json'
+
+    return response
+
+
+@app.route('/import', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        # if 'file' not in request.files:
+        #     # flash('No file part')
+        #     # return redirect(request.url)
+        file = request.files['import_data']
+        # # if user does not select file, browser also
+        # # submit a empty part without filename
+        # if file.filename == '':
+        #     flash('No selected file')
+        #     return redirect(request.url)
+        # if file and allowed_file(file.filename):
+        #     filename = secure_filename(file.filename)
+        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #     return redirect(url_for('uploaded_file',
+        #                             filename=filename))
+
+        if file:
+            print(file.read())
+            # TODO: Add decode logic here and submit to database.
+
+        return redirect(url_for('welcome'))
+    if request.method == 'GET':
+        return redirect(url_for('admin'))
 
 
 @app.route("/admin")
